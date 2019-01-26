5bdf4792dd7e077bb96ab808a16f9ea483978c4e
shen547 <shen8643@gmail.com>
2019-01-27 00:32:00 +0800

    sec

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -5,7 +5,7 @@ $(aws ecr get-login --no-include-email --registry-ids 204065533127 --region ap-n
 if [ `docker images | grep cc104devops-repo | wc -l`  = 1 ]
 then
         docker rmi cc104devops-repo
-        docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/cc104devops-repo:latest
+        docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/j12
 else
-        docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/cc104devops-repo:latest
+        docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/j12 
 fi
